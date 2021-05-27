6394a1c1f284763d10cf5655378ae3a0f60865e4
Daniel <danielmarroquin5224@gmail.com>
2021-05-26 20:01:53 -0500

    adding files for github pages

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Scott Daniel',
+    username: 'Scott',
+    email: 'sdb5224@gmail.com',
+  },
+  {
+    id: 2,
+    name: 'Daniel Barrera',
+    username: 'Daniel',
+    email: 'daniel@gmail.com',
+  },
+  {
+    id: 3,
+    name: 'Alesandro Daniel',
+    username: 'Alessandro',
+    email: 'alessandro@gmail.com',
+  },
+  {
+    id: 4,
+    name: 'Cesar Duarte',
+    username: 'Cesar',
+    email: 'cesar@gmail.com',
+  },
+  {
+    id: 5,
+    name: 'Pedro Ramirez',
+    username: 'Pedro',
+    email: 'pedro@gmail.com',
+  },
+  {
+    id: 6,
+    name: 'John Wilcock',
+    username: 'John',
+    email: 'john@gmail.com',
+  },
+  {
+    id: 7,
+    name: 'Will Daniel',
+    username: 'Will',
+    email: 'will@gmail.com',
+  },
+  {
+    id: 8,
+    name: 'Tom Carter',
+    username: 'Tom',
+    email: 'tom@gmail.com',
+  },
+  {
+    id: 9,
+    name: 'Diana Salcedo',
+    username: 'Diana',
+    email: 'diana@gmail.com',
+  },
+  {
+    id: 10,
+    name: 'Jaime Bonilla',
+    username: 'Jaime',
+    email: 'jaime@gmail.com',
+  },
+];
